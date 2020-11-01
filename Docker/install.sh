8c5b4fc9d169e21352977a786145eeb6797e8638
Sean Alborough <salborough@gmail.com>
2020-11-01 09:59:36 +0200

    new changes

--- Docker/install.sh
+++ Docker/install.sh
@@ -19,15 +19,18 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # use the following to verify the add succeeded
 # sudo apt-key fingerprint 0EBFCD88
 
-#5 get the lastest stable release of docker
+#5 add the coker repository to our pat-get repo list  so we can get the lastest stable release of docker
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
+#6 run an update on the apt-get repo
 sudo apt-get update
 
+#7 get and install docker (community edition cli and container io to run containers)
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+#8 this is pulling down this nyancat image from docker hub as we going to use this in our tutorial
 # https://hub.docker.com/r/06kellyjac/nyancat/
 sudo docker run -it --rm --name nyancat 06kellyjac/nyancat
